91ec04a713c35327c53e5037d49ddc1eaf651e96
datum-dpoulin <dpoulin@datumtc.ca>
2022-07-15 21:56:48 -0400

    Update for new MIO API

--- dv/uvml_mem/src/obj/uvml_mem_model.sv
+++ dv/uvml_mem/src/obj/uvml_mem_model.sv
@@ -1,15 +1,8 @@
 // Copyright 2021 OpenHW Group
 // Copyright 2021 Datum Technology Corporation
 // Copyright 2021 Silicon Labs
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
-// Licensed under the Solderpad Hardware License v 2.1 (the "License"); you may not use this file except in compliance
-// with the License, or, at your option, the Apache License version 2.0.  You may obtain a copy of the License at
-//                                        https://solderpad.org/licenses/SHL-2.1/
-// Unless required by applicable law or agreed to in writing, any work distributed under the License is distributed on
-// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations under the License.
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 `ifndef __UVML_MEM_MODEL_SV__
@@ -22,10 +15,10 @@
 class uvml_mem_model_c #(
    int unsigned XLEN = `UVML_MEM_DEFAULT_XLEN
 ) extends uvm_object;
-   
-   rand      uvml_mem_default_val_enum  mem_default            ; ///< 
-   rand      bit[7:0]                   mem_default_const_value; ///< 
-   protected logic[7:0]                 _mem[bit[XLEN-1:0]]    ; ///< 
+
+   rand      uvml_mem_default_val_enum  mem_default            ; ///<
+   rand      bit[7:0]                   mem_default_const_value; ///<
+   protected logic[7:0]                 _mem[bit[XLEN-1:0]]    ; ///<
 
 
    `uvm_object_utils_begin(uvml_mem_model_c);
@@ -38,13 +31,13 @@ class uvml_mem_model_c #(
       // By default avoid X's from this memory, can be overriden by core config constraints if X's are desired for some reason
       soft mem_default != UVML_MEM_DEFAULT_VAL_X;
    }
-   
-   
+
+
    /**
     * Default constructor
     */
    extern function new(string name="uvml_mem_model");
-  
+
    /**
     * Write to memory array
     */
@@ -64,9 +57,9 @@ endclass : uvml_mem_model_c
 
 
 function uvml_mem_model_c::new(string name="uvml_mem_model");
-   
+
    super.new(name);
-  
+
 endfunction : new
 
 
@@ -78,7 +71,7 @@ endfunction : write
 
 
 function logic[7:0] uvml_mem_model_c::read(bit[XLEN-1:0] addr);
-   
+
    if (!_mem.exists(addr)) begin
       case (mem_default)
          UVML_MEM_DEFAULT_VAL_X:      _mem[addr] = 'x;
@@ -95,7 +88,7 @@ endfunction : read
 
 
 function void uvml_mem_model_c::readmemh(string mem_file_path);
-   
+
    string error_desc;
    int file  = $fopen(mem_file_path, "r");
    int errno = $ferror(file, error_desc);
